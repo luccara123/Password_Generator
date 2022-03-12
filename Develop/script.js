c4ff4b1c698d9a25feb3b19d3a76f70af7f8e9d1
Lucca Araujo <lucca.ara7@gmail.com>
2022-03-11 22:31:15 -0800

    adding the steps for the project and the prompt code

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,34 @@
 // Assignment code here
 
+//steps:
+// Make the generatePassword function
+// 1.Prompt the user the questions
+//  - password length 8 < 128
+//  - Lowercase, uppercase, number and special chracters
+//  -  Validade the imput, if one type is selected, then is validaded
+// Return the variable that storared the password value
+// 2. Display password for the page
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// 
+function generatePassword(){
+  var passwordCharacters = prompt("Please enter how many characters for the password")
+  
+    if (passwordCharacters < 8 || passwordCharacters > 128  ){ //add if a string
+      alert("Not valid! Enter a number between 8 and 128")
+    } else if(passwordCharacters >= 8 && passwordCharacters <= 128) {
+      var lowerCaseConfirmed = confirm("Add lowercase letters?");
+      var upperCaseConfirmed = confirm("Add uppercase letters?");
+      var numbersConfirmed = confirm("Add numbers?");
+      var specialCharactersConfirmed = confirm("Add special characters?");
+    }
+}
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
